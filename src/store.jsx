c3931b36627fe19291b8aa497f28ef4a1cbdd76e
Danish As-Salafy Rachman <129130056+DanishRach@users.noreply.github.com>
2024-12-16 09:34:19 +0700

    another commit

--- src/store.jsx
+++ src/store.jsx
@@ -45,7 +45,7 @@ const state = {
       aspect: 1.5037,
       text: "This is a personal project inspired by a Three.js example, where I attempted to recreate a space-themed game. The game features immersive sound effects, a shooting system, and fully 3D-designed assets. I used Three.js to bring the visual elements to life, showcasing my ability to work with 3D graphics and interactive features.",
       link1: "https://github.com/DanishRach/3Dcar",
-      link2: "3d-car-two.vercel.app",
+      link2: "https://3d-car-two.vercel.app/",
     },
   ],
   stripes: [
